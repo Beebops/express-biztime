85b286376b4780e1c656457f3f4007ca60b014c3
Beth Carretta <bethcarretta@gmail.com>
2023-09-01 13:28:54 -0400

    slugified code

--- routes/companies.js
+++ routes/companies.js
@@ -2,6 +2,7 @@ const express = require('express')
 const ExpressError = require('../expressError')
 const router = express.Router()
 const db = require('../db')
+const slugify = require('slugify')
 
 /** Returns list of companies, like {companies: [{code, name}, ...]} */
 router.get('/', async (req, res, next) => {
@@ -48,7 +49,9 @@ router.get('/:code', async (req, res, next) => {
 /** Adds a new company to db. Returns obj of new company: {company: {code, name, description}} */
 router.post('/', async (req, res, next) => {
   try {
-    const { code, name, description } = req.body
+    const { name, description } = req.body
+    const code = slugify(name, { lower: true })
+
     const result = await db.query(
       `INSERT INTO companies (code, name, description)
         VALUES ($1, $2, $3)
